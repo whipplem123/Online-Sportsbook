66f8be9bd0e96315b9e8a26aa574ec34ad62d43b
whipplem123 <32418238+whipplem123@users.noreply.github.com>
2018-04-25 15:33:17 -0400

    Created updater.py to update bets based on results, modified line creator

--- LineCreator.py
+++ LineCreator.py
@@ -8,43 +8,39 @@ cursor = conn.cursor(buffered=True)
 cursor.execute("select home_id, away_id, date from nba_schedule")
 
 for (home_id, away_id, date) in cursor:
-        cursor2 = conn.cursor(buffered=True)
-        cursor3 = conn.cursor(buffered=True)
-        cursor2.execute("select points_for, points_against from nba where team_id = %s", (home_id,))
-        cursor3.execute("select points_for, points_against from nba where team_id = %s", (away_id,))
-        homeDiff = 0.0
-        awayDiff = 0.0
-        over_under = 0.0
-        homeML = 0
-        awayML = 0
-        for (points_for, points_against) in cursor2:
-                over_under += points_for + points_against
-                homeDiff = points_for - points_against
+	cursor2 = conn.cursor(buffered=True)
+	cursor3 = conn.cursor(buffered=True)
+	cursor2.execute("select points_for, points_against from nba where team_id = %s", (home_id,))
+	cursor3.execute("select points_for, points_against from nba where team_id = %s", (away_id,))
+	homeDiff = 0.0
+	awayDiff = 0.0
+	over_under = 0.0
+	homeML = 0
+	awayML = 0
+	for (points_for, points_against) in cursor2:
+		over_under += points_for + points_against
+		homeDiff = points_for - points_against
 
-        for (points_for, points_against) in cursor3:
-                over_under += points_for + points_against
-                awayDiff = points_for - points_against
+	for (points_for, points_against) in cursor3:
+		over_under += points_for + points_against
+		awayDiff = points_for - points_against
 
-        homeSpread = math.floor(awayDiff - homeDiff - 3.0) + 0.5
-        awaySpread = -1 * homeSpread
+	homeSpread = math.floor(awayDiff - homeDiff - 3.0) + 0.5
+	awaySpread = -1 * homeSpread
 
-        if homeSpread <= 0:
-                x = .5 - homeSpread * .03
-                homeML = math.floor((-100 * x) / (1-x))
-        else:
-                x = .5 - homeSpread * .03
-                homeML = math.floor((100 - 100 * x) / x)
+	if homeSpread <= 0:
+		x = .5 - homeSpread * .03
+		homeML = int(round(math.floor((-100 * x) / (1-x)), -1))
+	else:
+		x = .5 - homeSpread * .03
+		homeML = int(round(math.floor((100 - 100 * x) / x), -1))
 
-		if awaySpread <= 0:
-                x = .5 - awaySpread * .03
-                awayML = math.floor((-100 * x) / (1-x))
-        else:
-                x = .5 - awaySpread * .03
-                awayML = math.floor((100 - 100 * x) / x)
+	awayML = -1 * homeML
 
-        over_under /= 2.0
-        cursor4 = conn.cursor()
-        cursor4.execute("update nba_schedule set home_spread = %s, home_money_line = %s, away_spread = %s, away_money_line = %s, over_under = %s where (home_id = %s and date = %s)", (homeSpread, homeML, awaySpread, awayML, over_under, home_id, date,))
-        conn.commit()
+	over_under /= 2.0
+	over_under = math.floor(over_under) + 0.5
+	cursor4 = conn.cursor()
+	cursor4.execute("update nba_schedule set home_spread = %s, home_money_line = %s, away_spread = %s, away_money_line = %s, over_under = %s where (home_id = %s and date = %s)", (homeSpread, homeML, awaySpread, awayML, over_under, home_id, date,))
+	conn.commit()
 
 conn.close()
